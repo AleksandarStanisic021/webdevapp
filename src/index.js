46c64dff937b26da9c1719f033b9c48b6200d918
ciandre1976 <ciandre23@gmail.com>
2026-01-29 22:34:25 +0100

    Add CORS support and connect to MongoDB in the application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import {app} from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./.env"
@@ -8,7 +9,12 @@ console.log(process.env.PORT);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}..`);
+connectDB().then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}..`);
+    });
+}).catch((err) => {
+    console.log(err);
 });
 
