11241fda943a6b84fbf951bf0780c864868d961e
ciandre1976 <ciandre23@gmail.com>
2026-02-06 03:09:57 +0100

    fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -27,7 +27,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const user = await User.create({
         fullname,
-        avatar: avatar.url,
+        avatar: avatar.url || "",
         coverImage: coverImage.url || "",
         email,
         password,
